4b6e80ce184ea2038bf5c1028f640cc992588125
cybersmission <77613871+cybersmission@users.noreply.github.com>
2021-06-23 11:50:37 +0530

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -29,7 +29,7 @@ echo -e $'\e[1;31m                                                              
 echo -e $'\e[1;36m                                                                Twitter - https://twitter.com/Cyber_mission/\e[0m'
 echo""
 echo -e $'\e[1;31m \e[0m\e[1;33m\e[0m'"\e[96m"'\e[1;33m\e[0m\e[1;31m\e[0m'
-echo -e $'\e[1;32m                                                                     BY \e[0m''\e[1;31mℂ\e[0m\e[1;34m𝕐𝔹𝔼ℝ \e[1;31m𝕄\e[1;34m 𝕀𝕊𝕊𝕀𝕆ℕ \e[0m'                                       
+echo -e $'\e[1;32m                                                                     BY \e[0m''\e[1;31mC\e[0m\e[1;34mYBER \e[1;31mM\e[1;34m ISSION \e[0m'                                       
  echo""
  echo""
  echo -e $'\e[1;31m   _______  _____ _________   _     _ ____ ____ _ ____ _     \e[0m'
@@ -37,7 +37,7 @@ echo -e $'\e[1;32m                                                              
  echo -e $'\e[1;31m  |  /  \  /| | //  \ |  \/| | |\/|| |    \    \ | / \| |\ ||\e[0m'
  echo -e $'\e[1;31m  |  \__/ / | |_\\  /_|    / | |  || |___ |___ | | \_/| | \||\e[0m'
  echo -e $'\e[1;31m  \____/_/  \____/____\_/\_\ \_/  \|_/____/____/_/____/_/  \|\e[0m'
- echo -e $'\e[1;31m  \e[1;32m𝑯𝑨𝑪𝑲 𝑺𝑶𝑪𝑰𝑨𝑳 𝑨𝑪𝑪𝑶𝑼𝑵𝑻 \e[0m'
+ echo -e $'\e[1;31m  \e[1;32mHACK SOCIAL ACCOUNTS \e[0m'
   echo""
   echo ""
   echo ""
